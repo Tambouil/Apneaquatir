41e8f63ed0115cb64c2fab86cf5cf42abfd73329
Tambouil <v.berceaux@gmail.com>
2024-02-20 17:49:31 +0100

    chore: setup tailwind

--- resources/views/pages/home.tsx
+++ resources/views/pages/home.tsx
@@ -3,7 +3,7 @@ import { Layout } from '#views/layouts/app'
 export function Home() {
   return (
     <Layout>
-      <h1 class="test">Hello World</h1>
+      <h1 class="text-3xl font-bold underline text-red-500">Hello World</h1>
     </Layout>
   )
 }
