c001313de6a963313a5ad1b4b49a5b1266aa2e51
Tambouil <v.berceaux@gmail.com>
2024-03-02 19:17:50 +0100

    feat: setup alpine and unpoly for interactivity

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -1,4 +1,4 @@
-import vine, { SimpleMessagesProvider } from '@vinejs/vine'
+import vine from '@vinejs/vine'
 
 export const createRegisterValidator = vine.compile(
   vine.object({
@@ -10,19 +10,3 @@ export const createRegisterValidator = vine.compile(
     }),
   })
 )
-vine.messagesProvider = new SimpleMessagesProvider({
-  // Applicable for all fields
-  'required': 'The {{ field }} field est requis',
-  'string': 'Le champs {{ field }} doit être une chaine de caractères',
-  'email': "Le champs n'est pas une adresse email valide",
-
-  // Error message for the username field
-  'firstName.required': `Le nom d\'utilisateur est requis`,
-  'username.unique': `Ce nom d'utilisateur est déjà utilisé`,
-  'email.required': `L'adresse email est requise`,
-  'email.email': `L'adresse email n'est pas valide`,
-  'email.unique': `Cette adresse email est déjà utilisée`,
-  'password.required': `Le mot de passe est requis`,
-  'password.confirmed': `Les mots de passe ne correspondent pas`,
-  'password.minLength': `Le mot de passe doit contenir au moins 6 caractères`,
-})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ['./resources/**/*.edge', './resources/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'system-ui', 'sans-serif'],
+    },
+    boxShadow: {
+      DEFAULT: '0px 2px 4px rgba(44, 43, 42, 0.1)',
+      spread: '0px 12px 32px rgba(44, 43, 42, 0.16)',
+    },
+    minWidth: {
+      240: '240px',
+    },
     extend: {},
   },
   // eslint-disable-next-line unicorn/prefer-module
